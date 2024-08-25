7960cf58e968bbd3091e4534f1bc6a46cbce3294
mgjain9871 <mitulgoeljain0123@gmail.com>
2024-08-25 21:43:29 +0530

    initial commit.

---
+++ src/main/java/com/example/ipl_dashboard/repository/TeamRepository.java
@@ -0,0 +1,13 @@
+package com.example.ipl_dashboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ipl_dashboard.model.Team;
+
+@Repository
+public interface TeamRepository extends CrudRepository<Team, Long>{
+
+	Team findByTeamName(String teamName);
+	
+}
